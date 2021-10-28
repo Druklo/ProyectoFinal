88098c5065f114bad6ab9e288a75a63be2c9cd25
PabloTiche <82000703+PabloTiche@users.noreply.github.com>
2021-10-28 16:39:37 -0300

    Update template.php

--- ProyectoFinal/Vistas/template.php
+++ ProyectoFinal/Vistas/template.php
@@ -20,7 +20,7 @@
         <div class="row">
             <div class="col-12">
                 <?php
-                    $controlador = new ControladorDePaginas();
+                    $controlador = new ControladorPaginas();
                     $controlador -> obtenerPagina();
                 ?>
                 
