13f8a43299559564c258f2c8480c5df9c093d2c1
Pablo Cladera <83961694+Druklo@users.noreply.github.com>
2021-10-28 16:10:29 -0300

    Add files via upload

---
+++ ProyectoFinal/Controladores/ControladorDePaginas.php
@@ -0,0 +1,80 @@
+<?php
+    include_once "Conexion.php";
+    include_once "Modelos/Vehiculo.php";
+
+    class ControladorPaginas
+    {
+
+        public function plantilla(){
+            include_once "Vistas/template.php";
+        }
+
+        public function obtenerPagina()
+        {
+
+            if( isset($_GET['accion']) )
+            {
+                
+                $accion = $_GET['accion'];
+                
+                return $this->$accion();
+            }
+
+        }
+
+
+        private function inicio()
+        {
+            $productos = Vehiculo::consultar();
+
+            return include_once "Vistas/Vehiculo/Inicio.php";
+        }
+
+        private function registrar(){
+
+            if($_POST)
+            {
+                $marca = $_POST['marca'];
+                $modelo = $_POST['modelo'];
+                $anio = $_POST['año'];
+                $precio = $_POST['precio'];
+                
+                Vehiculo::registrar($marca, $modelo, $anio, $precio);
+            }
+            return include_once "Vistas/Productos/RegistrarAuto.php";
+        }
+
+        private function editar(){
+            if(isset($_GET['id'])){
+
+                $vehiculo =  Vehiculo::buscar($_GET['id']);
+            }
+
+            if($_POST){
+                print_r($_POST);
+                $id = $_POST['id'];
+                $marca = $_POST['marca'];
+                $modelo = $_POST['modelo'];
+                $anio = $_POST['año'];
+                $precio = $_POST['precio'];
+
+                Vehiculo::editar($id, $marca, $modelo, $anio, $precio);
+
+                header("Location:./?accion=inicio");
+            }
+
+            return include_once "Vistas/Vehiculo/editar.php";
+            
+        }
+
+        private function borrar(){
+            if(isset($_GET['id'])){
+                Vehiculo::borrar($_GET['id']);
+
+                header("Location:./?accion=inicio");
+            }
+            
+        }
+    }
+
+?>
--- ProyectoFinal/Modelos/Vehiculo.php
+++ ProyectoFinal/Modelos/Vehiculo.php
@@ -1,6 +1,8 @@
 <?php
 
-    class Vehiculo
+use Vehiculo as GlobalVehiculo;
+
+class Vehiculo
     {
 
         public $Id;
@@ -8,7 +10,6 @@
         public $Modelo;
         public $Anio;
         public $Precio;
-        //catidad ??
 
         public function __construct($id, $marca, $modelo, $anio, $precio)
         {
@@ -17,6 +18,98 @@
             $this->Modelo = $modelo;
             $this->Anio = $anio;
             $this->Precio = $precio;
+        }
+
+        public static function consultar()
+        {
+            $listaVehiculos = [];
+            $conexion = BD::crearConexion();
+            $consulta = "SELECT * FROM vehiculos";
+            if ($resultado = mysqli_query($conexion, $consulta)) 
+            {
+                while ($vehiculo= $resultado->fetch_object())
+                {
+
+                    $listaVehiculos[] = new Vehiculo($vehiculo->Id, $vehiculo->Marca, $vehiculo->Modelo,$vehiculo->Anio, $vehiculo->Precio);
+                }
+            }
+
+            return $listaVehiculos;
+        }
+
+        public static function borrar($id)
+        {
+            $conexion = BD::crearConexion();
+            $query = "DELETE FROM vehiculos WHERE id = '$id'";
+            $exito = mysqli_query($conexion, $query);
+
+            if(!$exito)
+            {
+                echo "Hubo un error al eliminar el vehiculo: ".mysqli_error($conexion);
+            }
+        }
+
+        public static function editar($id, $marca, $modelo, $anio, $precio)
+        {
+            $conexion = BD::crearConexion();
+            $query = "UPDATE vehiculos SET 
+                                Marca ='$marca', 
+                                Modelo='$modelo', 
+                                Año='$anio', 
+                                Precio='$precio'
+                                WHERE id = '$id'";
+            $exito = mysqli_query($conexion, $query);
+
+            if(!$exito)
+            {
+                echo "Hubo un error al actualizar el vehiculo: ".mysqli_error($conexion);
+            }
+        }
+
+        public static function buscar($id)
+        {
+
+            $conexion = BD::crearConexion();
+
+            $query = "SELECT * FROM vehiculos WHERE id = '$id' ";
+
+            $resultado = mysqli_query($conexion, $query);
+
+            if($resultado)
+            {
+                if (mysqli_num_rows($resultado) > 0)
+                {
+                    $vehiculo = $resultado->fetch_object();
+
+                    return new Vehiculo($vehiculo->Id, $vehiculo->Marca, $vehiculo->Modelo,$vehiculo->Anio, $vehiculo->Precio);
+                }
+                else
+                {
+                    echo "El vehiculo con ese ID no se encontro";
+                }
+                
+            }
+            else
+            {
+                echo "Hubo un error al buscar el vehiculo: ".mysqli_error($conexion);
+            }
+        }
+
+        public static function registrar($marca, $modelo, $anio, $precio)
+        {
+            
+            $conexion = BD::crearConexion();
+
+            // Codigo SQL para insertar datos en la tabla personas 
+            $query = "INSERT INTO productos (Nombre, Precio, Cantidad) values ('$marca', '$modelo', '$anio', '$precio')";
+            $exito = mysqli_query($conexion, $query);
+            
+            if($exito){
+                echo "Se guardaron correctamente los datos";
+            }
+            else{
+                echo "Hubo un error al guardar los datos".mysqli_error($conexion);
+            }
         }
     }
 
