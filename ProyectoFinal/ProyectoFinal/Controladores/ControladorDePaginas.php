51372473dd3802511f19999d8f90d2fea3e2c716
Pablo Cladera <83961694+Druklo@users.noreply.github.com>
2021-11-15 19:22:58 -0300

    Add files via upload

---
+++ ProyectoFinal/ProyectoFinal/Controladores/ControladorDePaginas.php
@@ -0,0 +1,83 @@
+<?php
+    include_once "Conexion.php";
+    include_once "Modelos/Vehiculo.php";
+
+    class ControladorPaginas
+    {
+        public function plantilla()
+        {
+        include_once "Vistas/Template.php";
+        }
+
+        public function obtenerPagina()
+        {
+
+            if( isset($_GET['accion']) )
+            {
+                $accion = $_GET['accion'];
+                
+                return $this->$accion();
+            }
+
+        }
+
+
+        private function inicio()
+        {
+            $vehiculos = Vehiculo::consultar();
+
+            return include_once "Vistas/Vehiculo/Inicio.php";
+        }
+
+        private function registrar(){
+
+            if($_POST)
+            {
+                //$id = $_POST ['ID'];
+                $marca = $_POST['marca'];
+                $modelo = $_POST['modelo'];
+                $anio = $_POST['anio'];
+                $precio = $_POST['precio'];
+                
+                Vehiculo::registrar($marca, $modelo, $anio, $precio);
+            }
+            return include_once "Vistas/Vehiculo/RegistrarAuto.php";
+        }
+
+        private function editar(){
+            if(isset($_GET['id'])){
+
+                $vehiculo =  Vehiculo::buscar($_GET['id']);
+            }
+
+            if($_POST)
+            {
+                print_r($_POST);
+                $id = $_POST['id'];
+                $marca = $_POST['marca'];
+                $modelo = $_POST['modelo'];
+                $anio = $_POST['anio'];
+                $precio = $_POST['precio'];
+
+                Vehiculo::editar($id, $marca, $modelo, $anio, $precio);
+
+                header("Location:./?accion=inicio");
+            }
+
+            return include_once "Vistas/Vehiculo/editar.php";
+            
+        }
+
+        private function borrar()
+        {
+            if(isset($_GET['id']))
+            {
+                Vehiculo::borrar($_GET['id']);
+
+                header("Location:./?accion=inicio");
+            }
+            
+        }
+    }
+
+?>
